293d66b937f07971941da846617842af2176ce70
GENDOSMAL <33393060+GENDOSMAL@users.noreply.github.com>
2019-03-30 13:33:38 +0300

    Create project of restAPI

---
+++ Samlples/RepairFlatRestApi/RepairFlatRestApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RepairFlatRestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Samlples/RepairFlatRestApi/RepairFlatRestApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RepairFlatRestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
