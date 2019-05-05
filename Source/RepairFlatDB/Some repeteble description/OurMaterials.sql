d42253c1ca9d6df913d3dac7a929e06b166ed648
GENDOSMAL <GENDOSMAL@users.noreply.github.com>
2019-05-05 05:20:15 +0300

    Make some beatuful

--- Source/RepairFlatDB/Some repeteble description/OurMaterials.sql
+++ Source/RepairFlatDB/Some repeteble description/OurMaterials.sql
@@ -4,5 +4,6 @@
     [NameOfMaterial] NCHAR(30) NULL, 
     [UnitOfMeasue] NCHAR(10) NULL, 
     [Cost] MONEY NULL, 
-    [Description] NCHAR(100) NULL
+    [Description] NCHAR(100) NULL, 
+    [TypeOfMaterial] NCHAR(100) NULL
 )
---
+++ Source/RepairFlatDB/Worker/WorkersPayGive.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[WorkersPayGive]
+(
+	[idGive] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [idWorkerMake] UNIQUEIDENTIFIER NULL, 
+    [idWorkerAdresat] UNIQUEIDENTIFIER NULL, 
+    [Size] MONEY NULL, 
+    [Data] DATETIME2 NULL, 
+    [Descriptiom] NCHAR(100) NULL
+)
