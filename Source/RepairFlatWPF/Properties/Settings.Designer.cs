3033beaa1cbc8fcf56e324d5ec718140c731f4e9
GENDOSMAL <33393060+GENDOSMAL@users.noreply.github.com>
2019-06-20 08:07:14 +0300

    Make some update

--- Source/RepairFlatWPF/Properties/Settings.Designer.cs
+++ Source/RepairFlatWPF/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace RepairFlatWPF.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:26566")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://repflat1234.somee.com")]
         public string BaseAdress {
             get {
                 return ((string)(this["BaseAdress"]));
